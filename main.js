b0bfdc689da40a3234adbb9ca8e9e770db948aff
SpencerDWallace <007463307@coyote.csusb.edu>
2021-05-28 17:44:16 -0700

    Update to Quick Sort and Main sorting algo page

--- main.js
+++ main.js
@@ -18,9 +18,8 @@ function algos(){
     textStyle(NORMAL);
 
 
-    let projectHeader = createElement('h1', "Sorting Algorithms");
-    projectHeader.style('color', '#222222');
-    projectHeader.style('position', 'right')
+    let projectHeader = createElement('h1', "Sorting Algortithms");
+    projectHeader.position(_width*0.01, 0)
     homeDir =  createA('https://spencerdwallace.github.io/Home', 'Back to Home Directory', "_self");
     homeDir.position(_width/2 - textWidth('Back to Home Directory')/2, 30);
     quickSort = createA('https://spencerdwallace.github.io/sorting_algorithms/Quick%20Sort/', 'Quick Sort', "_self");
